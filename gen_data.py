62b1982e9cf38c751c89f4da9a33cca784abe17b
nicholasjhana <nicholas@nicholasjhana.com>
2019-05-08 14:40:02 +0000

    v1 working pipeline

---
+++ gen_data.py
@@ -0,0 +1,26 @@
+if __name__ == "__main__":
+	import get_data_verifly as gdf
+	import pandas as pd
+	import transform_metrics as tm
+	import datetime
+#
+	bucket = "verifly-adjust"
+#	day = 3
+	save_path = '/home/nick/adjust/data/verifly'
+#	days3 = datetime.date.today() - datetime.timedelta(days = 3)
+#	days2 = datetime.date.today() - datetime.timedelta(days = 2)
+
+	days = [2,3,4,5,6,7]
+
+	for day in days:
+		data = gdf.get_data_verifly(bucket, day, save_path)
+		metric = tm.calc_deliverables(data)
+		metric.to_csv('metrics' + str(day) + '.csv')
+
+	metrics = []
+	for day in days:
+		temp = pd.read_csv('metrics' + str(day) + '.csv')
+		metrics.append(temp)
+
+	data_metrics = pd.concat(metrics, ignore_index = True)
+	data_metrics.to_csv('vfly-deliverables.csv')
