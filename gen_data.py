d15083c891006667b088890775bf961fd6cad00f
nicholasjhana <nicholas@nicholasjhana.com>
2019-05-15 10:15:30 +0000

    data checking

--- gen_data.py
+++ gen_data.py
@@ -10,17 +10,13 @@ if __name__ == "__main__":
 #	days3 = datetime.date.today() - datetime.timedelta(days = 3)
 #	days2 = datetime.date.today() - datetime.timedelta(days = 2)
 
-	days = [2,3,4,5,6,7,8,9,10]
+	days = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+	metrics = []
 
 	for day in days:
 		data = gdf.get_data_verifly(bucket, day, save_path)
 		metric = tm.calc_deliverables(data)
-		metric.to_csv('metrics' + str(day) + '.csv')
-
-	metrics = []
-	for day in days:
-		temp = pd.read_csv('metrics' + str(day) + '.csv')
-		metrics.append(temp)
+		metrics.append(metric)
 
 	data_metrics = pd.concat(metrics, ignore_index = True)
 	data_metrics.to_csv('vfly-deliverables.csv')
